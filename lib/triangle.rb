f07073d781bb3c6dc4826c57cf2eadc4e447118d
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-07-16 00:54:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   
   
   def kind 
-     if sides.any? { |side| side <= 0 } || sides.any? == nil || sides[1]+sides[2] <
+     if sides.any? { |side| side <= 0 } || sides.any? == nil || sides[0]+sides[1] <=sides[2]
        raise TriangleError => error 
      elsif sides.uniq.length == 1
         :equilateral
